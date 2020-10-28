335989a0a18f63a04176a21528d0fd43fdc98a89
Makasuro <makasuro76@gmail.com>
2020-10-28 18:24:19 +0300

    добавил заготовки api

--- server.js
+++ server.js
@@ -1,11 +1,41 @@
+const { response } = require("express");
 const express = require("express");
-// создаем объект приложения
+// создаем сервер
 const srv = express();
 // определяем обработчик для маршрута "/"
-srv.get("/", function(request, response){
-     
-    // отправляем ответ
-    response.send("<h2>Привет Express!</h2>");
+srv.get("/", (request, response) => {
+    response.send("<h2>Здесь будет сайт водомера</h2>");
 });
-// начинаем прослушивать подключения на 3000 порту
+// получение текущего значения счетчика горячей воды
+srv.get("water/hot" , (request, response) => {
+    response.send({hot:1234});
+});
+
+// запись текущего значения счетчика горячей воды
+srv.post("water/hot" , (request, response) => {
+    response.status(200).send('Ok');
+});
+
+// получение текущего значения счетчика холодной воды
+srv.get("water/cold" , (request, response) => {
+    response.send({cold:1234});
+});
+
+// запись текущего значения счетчика холодной воды
+srv.post("water/cold" , (request, response) => {
+    response.status(200).send('Ok');
+});
+
+// получение значений счетчиков за предыдущий месяц
+srv.get("history/last" , (request, response) => {
+    response.send({hot:1234,cold:1234, date:'01.01.2020'});
+});
+// запись текущего состояния счетчиков в данном месяце
+srv.post("history/last" , (request, response) => {
+    response.status(200).send('Ok');
+});
+// получение истории за период для построения графиков
+srv.get("history/period" , (request, response) => {
+    response.send([{hot:1234,cold:1234, date:'01.01.2020'},{hot:1234,cold:1234, date:'01.02.2020'}]);
+});
 srv.listen(1337);
