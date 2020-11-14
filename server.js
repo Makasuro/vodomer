9709a22b9b363457617dc1ccacb653d9f3b9ade6
Victor Efimov <makasuro76@gmail.com>
2020-11-14 18:58:20 +0300

    добавил рассчет расхода

--- server.js
+++ server.js
@@ -17,42 +17,51 @@ srv.get("/", (request, response) => {
 });
 
 // получение текущего значения счетчика горячей воды
-srv.get("/water/hot" , async (request, response) => {
+srv.get("/api/water/hot" , async (request, response) => {
     response.send({hot: await db.GetCount('hot')});
 });
 
 // запись текущего значения счетчика горячей воды
-srv.post("/water/hot", jsonParser, (request, response) => {
+srv.post("/api/water/hot", jsonParser, (request, response) => {
     db.SetHotCount(request.body.hot);
     response.status(200).send();
 });
 
 // получение текущего значения счетчика холодной воды
-srv.get("/water/cold" , async (request, response) => {
+srv.get("/api/water/cold" , async (request, response) => {
        response.send({cold: await db.GetCount('cold')});
 });
 
 // запись текущего значения счетчика холодной воды
-srv.post("/water/cold", jsonParser, (request, response) => {
+srv.post("/api/water/cold", jsonParser, (request, response) => {
     db.SetColdCount(request.body.cold);
     response.status(200).send();
 });
 
 // получение значений счетчиков за предыдущий месяц
-srv.get("/history/last" , async (request, response) => {
+srv.get("/api/history/last" , async (request, response) => {
     const last = await db.GetLastRecord();
     response.status(200).send({hot: last.hot ,cold: last.cold , date: last.date });
 });
 
 // запись текущего состояния счетчиков в данном месяце
-srv.post("/history/last", jsonParser, async (request, response) => {
+srv.post("/api/history/last", jsonParser, async (request, response) => {
     await db.SetLastRecord(request.body.hot, request.body.cold);
     response.status(200).send('Ok');
 });
 
 // получение истории за период для построения графиков
-srv.get("/history/period" , async (request, response) => {
-    let result = await db.GetHistory();
+srv.get("/api/history" , async (request, response) => {
+    let result = await (await db.GetHistory()).map((item, index, items)=>{
+        if(index === 0) {
+        item.rateCold = 0;
+        item.rateHot = 0;
+        } else {
+            item.rateCold = item.cold - items[index-1].cold;
+            item.rateHot = item.hot - items[index-1].hot;
+        }
+        return item;
+    });
     response.send(result);
 });
 
